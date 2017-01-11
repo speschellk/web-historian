21c47b4c50e2d212d49a75c4cd320e322bf1bdc4
speschellk <kaychristensen@gmail.com>
2017-01-11 15:35:00 -0800

    Require archive-helpers file

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,14 @@
+// WORKER SERVER
+// I read archives/sites.txt for URLs that are added
+// I consult the internet to retrieve the index.html for those URLs
+// I add the index.html files to archives/sites/ (WEB SERVER picks up from there)
+
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+var archive = require('./archive-helpers');
+// archive.readListOfUrls
+// archive.isUrlInList
+// archive.addUrlToList
+// archive.isUrlArchived
+// archive.downloadUrls
